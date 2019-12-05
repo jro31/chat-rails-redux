145d0c0e4625bb192a864a7f09ffd2fc8d5c82a6
Jethro Williams <jethrowilliams4@hotmail.com>
2019-12-05 15:15:21 +0000

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Emptying the database..."
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "Generating channels..."
+channels = Channel.create([{ name: 'general' }, { name: 'London' }, { name: 'Ruby' }, { name: 'Rails' }, { name: 'React / Redux' }])
+
+puts "Generating users..."
+users = User.create([{ email: 'a@a.a', password: 'aaaaaaaa' }, { email: 'b@b.b', password: 'bbbbbbbb'}, { email: 'c@c.c', password: 'cccccccc' }])
+
+puts "Generating messages..."
+100.times do
+  Message.create([{channel: channels.sample, user: users.sample, content: Faker::GreekPhilosophers.quote}])
+end
